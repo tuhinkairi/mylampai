b5b9bb60032a9a66e32c586b32d26f407548651a
tuhinkairi <tuhinkairii@gmail.com>
2025-03-13 00:43:30 +0530

    job listing update

--- app/(protected)/(recruiter)/job/[jobProfileId]/page.tsx
+++ app/(protected)/(recruiter)/job/[jobProfileId]/page.tsx
@@ -1,17 +1,19 @@
 import { RoundsForm } from "./rounds-form";
 
-export default function JobProfilePage({
+export default async function JobProfilePage({
   params,
 }: {
   params: {
     jobProfileId: string;
   } 
 }) {
+  const param= await params;
+  const jobProfileId = param.jobProfileId;
   return (
     <div>
       <div>
         <h2>Add more rounds</h2>
-        <RoundsForm jobProfileId={params.jobProfileId}  />
+        <RoundsForm jobProfileId={jobProfileId}  />
       </div>
     </div>
   );
