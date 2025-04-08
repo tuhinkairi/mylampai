210027f4caae83714f3c561ba97ce85612e925db
tuhinkairi <tuhinkairii@gmail.com>
2025-04-08 22:41:32 +0530

    section 1 done

--- components/dashboard/jobdetails/nav-job-pages/BasicDetails.tsx
+++ components/dashboard/jobdetails/nav-job-pages/BasicDetails.tsx
@@ -1,4 +1,6 @@
+import { CalendarIcon, X } from "lucide-react";
 import React, { useState } from "react";
+import { RiEjectFill } from "react-icons/ri";
 
 type FormData = {
     jobTitle: string;
@@ -10,6 +12,7 @@ type FormData = {
     salaryFigure: string;
     jobDescription: string;
     employmentType: string;
+    expectedStartDate: string;
 };
 
 const BasicDetails = () => {
@@ -23,6 +26,7 @@ const BasicDetails = () => {
         salaryFigure: "",
         jobDescription: "",
         employmentType: "",
+        expectedStartDate: "",
     });
 
     const handleChange = (
@@ -33,12 +37,17 @@ const BasicDetails = () => {
 
     const handleSkillChange = (e: React.KeyboardEvent<HTMLInputElement>) => {
         if (e.key === "Enter" && e.currentTarget.value.trim()) {
-            setFormData((prevFormData) => ({
-                ...prevFormData,
-                skills: [...prevFormData.skills, e.currentTarget.value.trim()],
+            const newSkill = e.currentTarget.value.trim();
+            setFormData((prev) => ({
+                ...prev,
+                skills: [...prev.skills, newSkill],
             }));
             e.currentTarget.value = "";
         }
+    };
+
+    const handleStartDateSelection = (option: string) => {
+        setFormData((prev) => ({ ...prev, expectedStartDate: option }));
     };
 
     const handleSubmit = (e: React.FormEvent) => {
@@ -47,122 +56,160 @@ const BasicDetails = () => {
     };
 
     return (
-        <div className="p-2 flex justify-center ">
-            <form
+        <div className="p-2 flex justify-center text-sm min-w-full">
+            <form 
                 onSubmit={handleSubmit}
-                className="w-full border p-2 rounded-lg space-y-6 "
+                className="w-full border p-2 rounded-lg space-y-6"
             >
-                <label className="block">
-                    Job Title/Role:
-                    <input
-                        type="text"
-                        name="jobTitle"
-                        value={formData.jobTitle}
-                        onChange={handleChange}
-                        className="w-full p-2 mt-1  border  rounded-md"
-                    />
-                </label>
-                <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+                <section className="_part1 grid gap-4  border rounded-lg px-4 py-3">
                     <label className="block">
-                        Hiring Type:
-                        <div className="flex gap-4 mt-2">
+                        Job Title/Role:
+                        <input
+                            type="text"
+                            name="jobTitle"
+                            value={formData.jobTitle}
+                            onChange={handleChange}
+                            className="w-full p-2 mt-1 border rounded-md focus:outline-primary"
+                        />
+                    </label>
 
-                            <label className="flex items-center">
-                                <input
-                                    type="radio"
-                                    name="HiringType"
-                                    value="Job"
-                                    checked={formData.HiringType === "Job"}
-                                    onChange={handleChange}
-                                    className="mr-2"
-                                />
-                                Job
-                            </label>
-                            <label className="flex items-center">
-                                <input
-                                    type="radio"
-                                    name="HiringType"
-                                    value="Internship"
-                                    checked={formData.HiringType === "Internship"}
-                                    onChange={handleChange}
-                                    className="mr-2"
-                                />
-                                Internship
-                            </label>
-                            <label className="flex items-center">
-                                <input
-                                    type="radio"
-                                    name="HiringType"
-                                    value="Contract"
-                                    checked={formData.HiringType === "Contract"}
-                                    onChange={handleChange}
-                                    className="mr-2"
-                                />
-                                Contract
-                            </label>
+                    {/* Column 2 */}
+                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+                        {/* Hiring Type */}
+                        <label className="block">
+                            Hiring Type:
+                            <div className="flex gap-4 mt-2">
+                                {["Job", "Internship", "Contract"].map((type) => (
+                                    <label key={type} className="flex items-center">
+                                        <input
+                                            type="radio"
+                                            name="HiringType"
+                                            value={type}
+                                            checked={formData.HiringType === type}
+                                            onChange={handleChange}
+                                            className="mr-2 accent-primary checked:border-primary"
+                                        />
+                                        {type}
+                                    </label>
+                                ))}
+                            </div>
+                        </label>
+
+                        {/* Employment Type */}
+                        <label className="block">
+                            Employment Type:
+                            <select
+                                name="employmentType"
+                                value={formData.employmentType}
+                                onChange={handleChange}
+                                className="w-full p-2 mt-1 border rounded-md focus:outline-primary"
+                            >
+                                <option value="" disabled>
+                                    Select Employment Type
+                                </option>
+                                <option value="Full-Time">Full-Time</option>
+                                <option value="Part-Time">Part-Time</option>
+                            </select>
+                        </label>
+
+                        {/* Workplace Type */}
+                        <label className="block">
+                            Workplace Type:
+                            <select
+                                name="workplaceType"
+                                value={formData.workplaceType}
+                                onChange={handleChange}
+                                className="w-full p-2 mt-1 border rounded-md focus:outline-primary"
+                            >
+                                <option value="" disabled>
+                                    Select Workplace
+                                </option>
+                                <option value="In Office">In Office</option>
+                                <option value="Remote">Remote</option>
+                                <option value="Hybrid">Hybrid</option>
+                            </select>
+                        </label>
+                    </div>
+
+                    {/* Skills */}
+                    <label className="block">
+                        Skills Required:
+                        <input
+                            type="text"
+                            onKeyDown={handleSkillChange}
+                            className="w-full p-2 mt-1 border rounded-md focus:outline-primary"
+                        />
+                        <div className="mt-2 flex flex-wrap gap-2">
+                            {formData.skills.map((skill, index) => (
+                                <span key={index} className="px-2 py-1 border rounded-md border-primary flex items-center gap-3">
+                                    {skill} <button><X className="h-3 w-3" /></button>
+                                </span>
+                            ))}
                         </div>
                     </label>
 
-                    <label className="block">
-                        Employment Type:
-                        <select
-                            name="employmentType"
-                            value={formData.employmentType}
-                            onChange={handleChange}
-                            className="w-full p-2 mt-1 border rounded-md"
-                        >
-                            <option value="" disabled>
-                                Select Employment Type
-                            </option>
-                            <option value="Full-Time">Full-Time</option>
-                            <option value="Part-Time">Part-Time</option>
-                        </select>
-                    </label>
-                </div>
-
-                <label className="block">
-                    Skills (Press Enter to Add):
-                    <input
-                        type="text"
-                        onKeyDown={handleSkillChange}
-                        className="w-full p-2 mt-1  border  rounded-md"
-                    />
-                    <div className="mt-2 flex flex-wrap gap-2">
-                        {formData.skills.map((skill, index) => (
-                            <span key={index} className="px-2 py-1 bg-gray-700 rounded-md">
-                                {skill}
-                            </span>
-                        ))}
+                    {/* Expected Start Date */}
+                    <div className="rounded-lg">
+                        <p className="text-sm mb-2">Expected Start Date*</p>
+                        <div className="flex gap-4">
+                            {["Less than 7 days", "7-15 days", "After 15+ days"].map((option) => (
+                                <button
+                                    key={option}
+                                    onClick={() => handleStartDateSelection(option)}
+                                    className={`focus:outline-primary flex items-center px-4 py-2 border rounded-lg transition ${formData.expectedStartDate === option
+                                        ? "bg-primary text-white"
+                                        : "bg-inherit"
+                                        }`}
+                                >
+                                    <CalendarIcon className="w-4 h-4 mr-2" />
+                                    {option}
+                                </button>
+                            ))}
+                            <div className="ml-auto">
+                                <p className="text-xs text-gray-400 mb-1">Or Enter manually</p>
+                                <input
+                                    type="text"
+                                    name="expectedStartDate"
+                                    value={formData.expectedStartDate}
+                                    onChange={handleChange}
+                                    placeholder="in new season......."
+                                    className="px-4 py-2 border rounded-lg bg-transparent  focus:outline-none focus:outline-primary"
+                                />
+                            </div>
+                        </div>
                     </div>
-                </label>
+                </section>
 
-                <label className="block">
+                {/* Salary */}
+                {/* <label className="block">
                     Salary Figure (USD):
                     <input
                         type="number"
                         name="salaryFigure"
                         value={formData.salaryFigure}
                         onChange={handleChange}
-                        className="w-full p-2 mt-1  border  rounded-md"
+                        className="w-full p-2 mt-1 border rounded-md"
                     />
-                </label>
+                </label> */}
 
-                <label className="block">
+                {/* Job Description */}
+                {/* <label className="block">
                     Job Description:
                     <textarea
                         name="jobDescription"
                         value={formData.jobDescription}
                         onChange={handleChange}
-                        className="w-full p-2 mt-1  border  rounded-md"
+                        className="w-full p-2 mt-1 border rounded-md"
                         rows={4}
                     ></textarea>
-                </label>
+                </label> */}
 
+                {/* Action Buttons */}
                 <div className="flex justify-between">
-                    <button type="button" className="border  px-4 py-2 rounded-md">
+                    <button type="button" className="border bg-primary text-white hover:bg-primary-dark px-4 py-2 rounded-md">
                         Save as Draft
                     </button>
-                    <button type="submit" className="bg-white text-black px-4 py-2 rounded-md">
+                    <button type="submit" className=" px-4 py-2 rounded-md border bg-primary text-white hover:bg-primary-dark">
                         Publish
                     </button>
                 </div>
